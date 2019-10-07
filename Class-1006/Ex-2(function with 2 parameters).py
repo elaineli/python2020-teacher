a8202dc0105a513f58f2375162bf72786d25ad0b
elaine.li <elaine.bing.li@gmail.com>
2019-10-06 21:08:52 -0400

    Class exercises 1006

--- Class-1006/Ex-2(function with 2 parameters).py
+++ Class-1006/Ex-2(function with 2 parameters).py
@@ -1,2 +1,11 @@
 # First Write a function to repeat random string s n times, where s, n are the two parameters of this function
 # Then Call this function with the string and the repetition you like
+
+def repeatMe(str, numRepeats, name):
+    returnedStr = ''
+    for i in range(numRepeats):
+        returnedStr += (str + ":" + name) + '\n'
+    return returnedStr
+
+
+print(repeatMe('hello there', 10, 'elaine'))
--- Class-1006/Ex-2(function with loop).py
+++ Class-1006/Ex-2(function with loop).py
@@ -1,2 +1,9 @@
 # First Write a function to repeat hello world n times, where n is the argument of this function
 # Then Call this function with the argument passed as 20 to print print helloworld 20 times
+
+def hello(n):
+    for i in range(n):
+        print('hello world')
+
+
+hello(20)
